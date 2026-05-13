4c66c04dc16510dfd35cf4da995b1d00ff4031df
Dime98 <melniciucd@gmail.com>
2026-05-13 18:09:54 +0300

    Adds config utils

---
+++ cli_make_database.py
@@ -0,0 +1,74 @@
+import argparse
+import json
+import secrets
+import sys
+from pathlib import Path
+
+from rag_tool.pdf_utils.pdf_utils import extract_pdf_pages
+from rag_tool.config_utils.config_utils import (
+    load_config,
+    get_pdf_paths_from_config,
+    get_chunker_from_config,
+    get_encoder_from_config,
+)
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-config",
+        type=str,
+        required=True,
+        help="Path of configs for creating vector db.",
+    )
+    parser.add_argument(
+        "-save-to",
+        type=str,
+        required=True,
+        help="Path of configs for creating vector db.",
+    )
+    return parser.parse_args(args)
+
+
+if __name__ == "__main__":
+    args = parse_args(sys.argv[1:])
+
+    config = load_config(args.config)
+    pdf_file_paths = get_pdf_paths_from_config(config["pdf_paths"])
+    print(f"Found {len(pdf_file_paths)} .pdf file(s).")
+
+    chunking_method_config = config.get("chunking_method")
+
+    # save experiments output
+    save_to = Path(args.save_to)
+    experiment_folder = save_to / secrets.token_hex(8)
+    experiment_folder.mkdir(parents=True)
+    (experiment_folder / "config.json").write_text(json.dumps(config, indent=4))
+
+    pages = extract_pdf_pages(pdf_folders=pdf_file_paths)
+    print(f"Total number pages is '{len(pages)}'.")
+
+    text_chunker = get_chunker_from_config(config)
+    text_chunks = text_chunker.chunk(pages)
+    print("Done chunking text.")
+
+    texts, metadata, ids = [], [], []
+    for index, chunk in enumerate(text_chunks):
+        texts.append(chunk["text_chunk"])
+        metadata.append({"source": chunk["source"], "page": chunk["page"]})
+        ids.append(str(index))
+
+    encoder_method_config = config.get("encoder_method")
+    encoder = get_encoder_from_config(config)
+    embeddings = encoder.encode(texts)
+    print("Done encoding.")
+
+    import chromadb
+
+    chroma_client = chromadb.PersistentClient(experiment_folder / "chromadb")
+    collection = chroma_client.create_collection(
+        name=str(experiment_folder.stem),
+        metadata={"hnsw:space": "cosine"},
+    )
+    collection.add(documents=texts, embeddings=embeddings, metadatas=metadata, ids=ids)
+    print("Done creating vector DB.")
---
+++ rag_tool/config_utils/config_utils.py
@@ -0,0 +1,49 @@
+import json
+from pathlib import Path
+
+from rag_tool.encoder.encoder import Encoder
+from rag_tool.text_chunker.text_chunker import TextChunker
+
+
+def get_pdf_paths_from_config(pdf_paths: list[str]) -> list[Path]:
+    pdf_candidates = []
+    for file_path in pdf_paths:
+        file_path = Path(file_path)
+        if file_path.suffix.casefold() != ".pdf":
+            print(f"'{file_path}' is not of type '.pdf', therefore skipped.")
+            continue
+        if not file_path.exists():
+            print(f"'{file_path}' does not exist, therefore skipped.")
+            continue
+        pdf_candidates.append(file_path)
+    return pdf_candidates
+
+
+def load_config(config_path: str) -> dict:
+    config_file_path = Path(config_path)
+    if not config_file_path.exists():
+        raise FileNotFoundError(f"{config_file_path.exists()=}")
+    return json.loads(config_file_path.read_text())
+
+
+def get_chunker_from_config(config: dict) -> TextChunker:
+    chunking_config = get_config_field(config, "chunking_method")
+    return TextChunker.factory(
+        method=chunking_config.get("method"),
+        kwargs=chunking_config.get("kwargs"),
+    )
+
+
+def get_encoder_from_config(config: dict) -> Encoder:
+    encoder_config = get_config_field(config, "encoder_method")
+    return Encoder.factory(
+        method=encoder_config.get("method"),
+        kwargs=encoder_config.get("kwargs"),
+    )
+
+
+def get_config_field(config: dict, key: str) -> dict:
+    value = config.get(key)
+    if not value:
+        raise ValueError(f"Config is missing '{key}' field.")
+    return value
